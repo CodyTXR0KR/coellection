1216c92dee67a99f3b49131947856add679b4c9c
cody <codiferus17@gmail.com>
2016-09-12 22:19:20 -0400

    scrolling contributor marquee

---
+++ static/js/coellection.js
@@ -0,0 +1,81 @@
+/* Transform simple database strings to html link elements */
+function makeTwitterLink(username) {
+    var url = "https://twitter.com/" + username.slice(1);
+    return username.link(url);
+}
+
+function getTwitterAvatar(username) {
+    // Return the img src url for a twitter user
+    return "https://avatars.io/twitter/" + username.slice(1);
+}
+
+function donorOutput(donor_name) {
+    if (donor_name.startsWith("@")) {
+        return makeTwitterLink(donor_name);
+    };
+    return donor_name;
+}
+
+function populateTopDonor(input) {
+
+    var top_donor = input.split(" ");
+    var donor_name = top_donor[0];
+    var donation_count = top_donor.slice(-1)[0];
+
+    if (donor_name.startsWith("@")) {
+        var avatar = document.getElementById("avatar");
+        var link = document.getElementById("twitter_link");
+        var donations = document.getElementById("donations");
+
+        avatar.src = getTwitterAvatar(donor_name);
+        avatar.alt = donor_name;
+        link.innerHTML = makeTwitterLink(donor_name);
+        donations.innerText = donation_count + " donations!";
+    };
+    console.log("dynamic_links.js::populateTopDonor...done.");
+}
+
+// Scrolling contributor marquee
+// reference: http://stackoverflow.com/questions/10547797/very-simple-very-smooth-javascript-marquee
+function marquee(a, b) {
+    var width = b.width();
+    var start_pos = a.width();
+    var end_pos = -width;
+
+    function scroll() {
+        if (b.position().left <= -width) {
+            b.css('left', start_pos);
+            scroll();
+        }
+        else {
+            // Increase or decrease speed by changing value 50000
+            // larger is slower
+            time = (parseInt(b.position().left, 10) - end_pos) *
+                (50000 / (start_pos - end_pos));
+            b.animate({
+                'left': -width
+            }, time, 'linear', function() {
+                scroll();
+            });
+        }
+    }
+
+    b.css({
+        'width': width,
+        'left': start_pos
+    });
+    scroll(a, b);
+
+    b.mouseenter(function() {     // Remove these lines
+        b.stop();                 //
+        b.clearQueue();           // if you don't want
+    });                           //
+    b.mouseleave(function() {     // marquee to pause
+        scroll(a, b);             //
+    });                           // on mouse over
+
+}
+
+$(document).ready(function() {
+    marquee($('#contributors'), $('#donor_list'));
+});
